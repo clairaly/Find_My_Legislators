519cfc0ad2b55d0245097aacb5fca61cfb737896
“Alyssa <clairaly13@gmail.comgit config --global user.name clairaly>
2018-01-25 14:56:53 -0500

    laid down foundation for scraping

--- lib/find_my_legislators/legislators.rb
+++ lib/find_my_legislators/legislators.rb
@@ -2,27 +2,18 @@ class FindMyLegislators::Legislators
 
   attr_accessor :name, :party, :home, :district, :url
 
-  def self
-    puts <<-DOC.gsub /^\s*/, ''
-      1. Rep. First_Name Last_Name, political party, district number
-      2. Senator First_Name Last_Name, political party, district number
-    DOC
+  def self.legislators
+    #scrape whoismyrepresentative and then return legislators based on that data
+    self.scrape_legs
+  end
 
-    leg_1.self.new
-    leg_1.name = "Rep. Tim Walberg"
-    leg_1.party = "Republican"
-    leg_1.home = "Tipton, MI"
-    leg_1.district = "7"
-    leg_1.url = "https://whoismyrepresentative.com/member/view/W000798/"
+  def self.scrape_legs
+    legislators = []
+    #go to whoismyrepresentative, find the Legislators
+    #extract the properties
+    #instantiate a rep and senator
 
-    leg_2.self.new
-    leg_2.name = "Senator Stabenow"
-    leg_2.party = "Democrat"
-    leg_2.home = "Lansing, MI"
-    leg_2.district = "MI"
-    leg_2.url = "https://whoismyrepresentative.com/member/view/S000770/"
-
-    [leg_1, leg_2]
+    legislators
   end
 
 end
