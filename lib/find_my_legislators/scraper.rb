8863439baf0b589e930e78885267d5624343a4df
“Alyssa <clairaly13@gmail.comgit config --global user.name clairaly>
2018-02-16 17:37:40 -0500

    deleted scraper.rb file, no longer needed, legislators.rb does the scraping instead

--- lib/find_my_legislators/scraper.rb
+++
@@ -1,7 +0,0 @@
-class FindMyLegislators::Scraper
-
-    def self.scrape_legislator_page(zipcode)
-    	legislator_html = Nokogiri::HTML(open("https://whoismyrepresentative.com/home/" + zip_code ))
-    end
-
- end
